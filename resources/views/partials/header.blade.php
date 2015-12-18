a131c2e7e5c528f25c183bbfca04556e1b1ce0f3
Marcel Engelmann <engelmann.marcel@yahoo.de>
2015-12-18 23:27:17 +0100

    Update header

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -16,11 +16,27 @@
                 <li class="{{ (Request::is('/') ? 'active' : '') }}">
                     <a href="{{ URL::to('') }}">Home</a>
                 </li>
+                <li class="{{ (Request::is('features') ? 'active' : '') }}">
+                    <a href="{{ URL::to('features') }}">Features</a>
+                </li>
                 <li class="{{ (Request::is('about') ? 'active' : '') }}">
                     <a href="{{ URL::to('about') }}">About</a>
                 </li>
+                <li class="{{ (Request::is('blog') ? 'active' : '') }}">
+                    <a href="{{ URL::to('blog') }}">Blog</a>
+                </li>
                 <li class="{{ (Request::is('contact') ? 'active' : '') }}">
                     <a href="{{ URL::to('contact') }}">Contact</a>
+                </li>
+
+            </ul>
+
+            <ul class="nav navbar-nav navbar-right">
+                <li class="{{ (Request::is('auth/register/') ? 'active' : '') }}">
+                    <a href="{{ URL::to('auth/register/') }}">Join</a>
+                </li>
+                <li class="{{ (Request::is('auth/login/') ? 'active' : '') }}">
+                    <a href="{{ URL::to('auth/login/') }}">Login</a>
                 </li>
             </ul>
         </div>
