a2e4ea51c8a51a94547555437fa453ef9a106dd9
Marcel Engelmann <engelmann.marcel@yahoo.de>
2016-01-05 14:46:21 +0100

    Add Social Media Buttons to the button

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -1,13 +1,27 @@
 <footer>
     <div id="footer-newsletter">
         <div class="container">
-            <p>
+            <div class="pull-left">
                 {{ Lang::get('footer.newsletter_text') }}:
                 <form action="" method="post">
                     <input type="text" class="form-control form-footer-newsletter" placeholder="{{ Lang::get('footer.newsletter_email_placeholder') }}" />
                     <input type="submit" value="{{ Lang::get('footer.newsletter_submit_text') }}" name="subscribe" id="footer-newsletter-subscribe" class="btn btn-success">
                 </form>
-            </p>
+            </div>
+
+            <div class="pull-right">
+                <div id="footer-shraing">
+                    <!-- Twitter -->
+                    <a href="http://twitter.com/home?status=" title="Share on Twitter" target="_blank" class="btn btn-twitter"><i class="fa fa-twitter"></i> Twitter</a>
+                    <!-- Facebook -->
+                    <a href="https://www.facebook.com/sharer/sharer.php?u=" title="Share on Facebook" target="_blank" class="btn btn-facebook"><i class="fa fa-facebook"></i> Facebook</a>
+                    <!-- Google+ -->
+                    <a href="https://plus.google.com/share?url=" title="Share on Google+" target="_blank" class="btn btn-googleplus"><i class="fa fa-google-plus"></i> Google+</a>
+                    <!-- LinkedIn -->
+                    <a href="http://www.linkedin.com/shareArticle?mini=true&url=&title=&summary=" title="Share on LinkedIn" target="_blank" class="btn btn-linkedin"><i class="fa fa-linkedin"></i> LinkedIn</a>
+                </div>
+
+            </div>
         </div>
     </div>
 
